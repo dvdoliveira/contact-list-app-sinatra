3d7d20f0d08a40dba16794b2dfd62ab1861a3a87
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-02 00:49:28 +0000

    Add routes to actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,25 @@
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all.to_json
+end
+
+get '/contact/:id' do
+  @contact = Contact.find(params[:id]).to_json
+end
+
+post '/contact' do
+  @contact = Contact.new(firstname: params[:first_name], lastname: params[:last_name], email: params[:email], phone: params[:phone])
+  if @contact.save
+    @contact.to_json
+  else
+    errors.add(:base, "Could not create contact. Please try again.")
+  end
+end
+
+delete '/contact' do
+  @contact = Contact.find(params[:id])
+  @contact.destroy
+end
